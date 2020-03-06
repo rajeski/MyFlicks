5be80280f8a0f6defa96c8d95e55b74cc3a47173
rajeski <asiajsr@gmail.com>
2020-03-06 14:34:50 +0900

    Enable CORS for local react app/all domains

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,44 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// // client/src/index.jsx
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-// Import statement to indicate that you need to bundle `./index.scss`
-import './index.scss';
+// import { MainView } from './components/main-view/main-view';
 
-// Main component (will eventually use all the others)
-class MyFlixApplication extends React.Component {
-    render() {
-        return (
-            <div className="my-flix">
-                <div>Good morning</div>
-            </div>
-        );
-    }
-}
+// // Import statement to indicate that we need to bundle `./index.scss`
+// import './index.scss';
 
-// Finds the root of your app
-const container = document.getElementsByClassName('app-container')[0];
+// // Main component (will eventually use all the others)
+// class MyFlixApplication extends React.Component {
+//     render() {
+//         return <MainView />;
+//     }
+// }
 
-// Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+// // Find the root of our app
+// const container = document.getElementsByClassName('app-container')[0];
 
-// client/src/index.jsx
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { MainView } from './components/main-view/main-view';
-
-// Import statement to indicate that we need to bundle `./index.scss`
-import './index.scss';
-
-// Main component (will eventually use all the others)
-class MyFlixApplication extends React.Component {
-    render() {
-        return <MainView />;
-    }
-}
-
-// Find the root of our app
-const container = document.getElementsByClassName('app-container')[0];
-
-// Tell React to render our app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+// // Tell React to render our app in the root DOM element
+// ReactDOM.render(React.createElement(MyFlixApplication), container);
