b6aacb044fdcf01d7e88144ed3a224091bbd78dc
rajeski <asiajsr@gmail.com>
2020-04-02 21:46:09 +0900

    Updated registration-view.jsx file

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -36,40 +36,37 @@ export function RegistrationView(props) {
     };
 
     return (
-        <Container className='registrationContainer'>
-            <Form className='registrationForm'>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => createEmail(e.target.value)} />
-                    <Form.Text className='emailShare'>
-                        Your personal information will never be shared with any third-party.
-          </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId='formBasicUsername'>
-                    <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Username" value={username} onChange={e => createUsername(e.target.value)} />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Passwordy" value={password} onChange={e => createPassword(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group controlId='formBasicDob'>
-                    <Form.Label>Date of Birth</Form.Label>
-                    <Form.Control type="date" placeholder="01/01/1985" value={birthday} onChange={e => createDob(e.target.value)} />
-                </Form.Group>
-
-                <Form.Group controlId="formBasicChecbox">
-                    <Form.Check type="checkbox" label="Checking this means you are in good company" />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
-                    Get started!
-        </Button>
-                <Button variant='primary' onClick={() => props.onClick()}>
-                    Existing user?
-        </Button>
-            </Form>
+        <Container className="registrationForm">
+            <Card className="registration-card">
+                <h2 className="registration-title">Register</h2>
+                <Form>
+                    <Form.Group controlId="formBasicUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
+                        <Form.Text className="emailShare">
+                            Your personal information will never be shared!
+                </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicDob">
+                        <Form.Label>Date of Birth</Form.Label>
+                        <Form.Control type="date" value={birthdate} onChange={e => setBirthDate(e.target.value)} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" onClick={handleSubmit}>
+                        Sign-up
+              </Button>
+                    <Form.Text className="text-muted">
+                        Existing user? Log-in <Link to={"/"}>CLICK</Link>
+                    </Form.Text>
+                </Form>
+            </Card>
         </Container>
-    );
+    )
 }
