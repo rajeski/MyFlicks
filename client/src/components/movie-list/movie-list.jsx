aefed1a7d7fef5a24bf1be7e6535e11324c42e9f
rajeski <asiajsr@gmail.com>
2020-04-09 10:09:40 +0900

    Pre-commit revert

--- client/src/components/movie-list/movie-list.jsx
+++ client/src/components/movie-list/movie-list.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import { connect } from 'react-redux';
-
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import { MovieCard } from '../movie-card/movie-card';
 
 const mapStateToProps = state => {
-    const { visibilityFilter } = state;
-    return { visibilityFilter };
-};
-
-function MoviesList(props) {
-    const { movies, visibilityFilter } = props;
+    const { visibilityFilter, movies } = state;
     let filteredMovies = movies;
 
     if (visibilityFilter !== '') {
-        filteredMovies = movies.filter(m => m.Title.includes(visibilityFilter));
+        filteredMovies = filteredMovies.filter(m => m.title.toLowerCase().includes(visibilityFilter));
     }
 
-    if (!movies) return <div className="main-view" />;
+    return { movies: filteredMovies };
+};
 
-    return filteredMovies.map(m => <MovieCard key={m._id} movie={m} />);
+function MovieList(props) {
+    const { movies } = props;
+
+    if (!movies) {
+        return <div><p>... please stand-by... loading</p></div>;
+    }
+
+    return (
+        <Row>
+            {movies.map(movie => {
+                return (
+                    <Col key={movie._id} xs={12} sm={6} md={4}>
+                        <MovieCard
+                            key={movie._id}
+                            value={movie._id}
+                            movie={movie}
+                            addFavorites={movieId => this.addToFavorites(movieId)} />
+                    </Col>
+                );
+            })
+            }
+        </Row>
+    )
 }
 
-export default connect(mapStateToProps)(MoviesList);
+export default MovieList;
